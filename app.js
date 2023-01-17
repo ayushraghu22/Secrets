84fb4ce6086d33383f295716f6788cd64fd1a43b
Ayush <ayushraghuwanshi22@gmail.com>
2023-01-17 21:41:59 +0530

    Level - 4 Hashing and Salting with bcrypt

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-//jshint esversion:6
 require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+
 // const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -26,7 +28,7 @@ const userSchema = new mongoose.Schema({
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});    // adding the encrypt package as a plugin.
                                     // addding encryptedFields so that it don't encrypt the entire database.
 
-                                    
+
 const User = mongoose.model("User", userSchema);
 
 
@@ -44,14 +46,16 @@ app.get("/register", (req, res)=>{
 
 
 app.post("/register", (req, res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save((err)=>{
+            if(!err) res.render("secrets"); 
+            else console.log(err);
+        })
     });
-    newUser.save((err)=>{
-        if(!err) res.render("secrets"); 
-        else console.log(err);
-    })
 });
 
 app.post("/login", (req, res)=>{
@@ -60,10 +64,12 @@ app.post("/login", (req, res)=>{
         (err, foundUser)=>{
             if(!err){
                 if(foundUser){
-                    if(foundUser.password === md5(req.body.password))
-                        res.render("secrets");
-                    else
-                        res.send("Please enter correct username or password");
+                    bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+                        if(result)
+                            res.render("secrets");
+                        else 
+                            res.send(err);
+                    });
                 }
                 else 
                     res.send("Please enter correct username or password");
